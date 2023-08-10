7cb73b1558077da8c001e9c214c047bc24ce0418
Christopher Kinoshita <happychristopher777@gmail.com>
2023-08-10 11:31:38 -0700

    Fixed bug requiring entire form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -5,16 +5,16 @@ class FlightsController < ApplicationController
     if params[:flights]
       @flights = Flight.all
       # departure airports
-      if params[:flights][:departure_airport]
+      if params[:flights][:departure_airport] != ""
         # changed from @flights.where(departing_id: params[:flights][:departure_airport])
         @flights = @flights.where(departing_id: params[:flights][:departure_airport])
       end
       #arrival airports
-      if params[:flights][:arrival_airport]
+      if params[:flights][:arrival_airport] != ""
         @flights = @flights.where(arrival_id: params[:flights][:arrival_airport])
       end
       # start
-      if params[:flights][:'start(1i)'] && params[:flights][:'start(2i)'] && params[:flights][:'start(3i)']
+      if params[:flights][:'start(1i)'] != "" && params[:flights][:'start(2i)'] != "" && params[:flights][:'start(3i)'] != ""
         date = Date.new params[:flights][:'start(1i)'].to_i, params[:flights][:'start(2i)'].to_i, params[:flights][:'start(3i)'].to_i
         @flights = @flights.where("DATE(start) = ?", date)
       end
